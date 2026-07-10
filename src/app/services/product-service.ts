74b76e442e71f989a4435d9c5f42d5f6ef345b18
vgaudel <vgaudel@gmail.com>
2026-07-10 17:01:55 +0200

    ajout des endpoints du back dans des méthodes du productService

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -9,16 +9,48 @@ export class ProductService {
     private _http = inject(HttpClient);
     private readonly apiUrl = "http://localhost:3000/products"
 
-    getAllProducts$(): Observable<IProduct[]>{
-        return this._http.get<IProduct[]>(this.apiUrl);
-    }
+     getAllProducts$(): Observable<IProduct[]>{
+    return this._http.get<IProduct[]>(this.apiUrl);
+  }
+  
+  getProductById$(id: string): Observable<IProduct> {
+    return this._http.get<IProduct>(`${this.apiUrl}/${id}`);
+  }
 
-    createProduct$(product: Omit<IProduct, 'id'>) : Observable<IProduct>{
-        return this._http.post<IProduct>(this.apiUrl,product);
-    }
+  getProductByName$(name: string): Observable<IProduct> {
+    return this._http.get<IProduct>(`${this.apiUrl}/name/${name}`);
+  }
 
-    productExists$(name : string) : Observable<boolean>{
-        return this._http.get<boolean>(`${this.apiUrl}/exists/${name}`);
-    }
+  getProductsByCategory$(category: string): Observable<IProduct[]> {
+    return this._http.get<IProduct[]>(`${this.apiUrl}/category/${category}`);
+  }
+  
+  getProductsCategories$(): Observable<string[]> {
+    return this._http.get<string[]>(`${this.apiUrl}/categories`);
+  }
+
+  productExists$(name: string): Observable<boolean> {
+    return this._http.get<boolean>(`${this.apiUrl}/exists/${name}`);
+  }
+
+  countProducts$(): Observable<number> {
+    return this._http.get<number>(`${this.apiUrl}/count/total`);
+  }
+
+  createProduct$(product: Omit<IProduct, 'id'>): Observable<IProduct> {
+    return this._http.post<IProduct>(this.apiUrl, product);
+  }
+
+  updateProduct$(id: string, product: Omit<IProduct, 'id'>): Observable<IProduct> {
+    return this._http.put<IProduct>(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProductById$(id: string): Observable<{ message: string }> {
+    return this._http.delete<{ message: string }>(`${this.apiUrl}/${id}`);
+  }
+
+  deleteProductByName$(name: string): Observable<{ message: string }> {
+    return this._http.delete<{ message: string }>(`${this.apiUrl}/name/${name}`);
+  }
 
 }
